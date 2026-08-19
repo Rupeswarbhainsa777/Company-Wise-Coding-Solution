d0d36ac8d805675a0ce4b896e7d5f505e56ac3da
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-08-19 22:46:04 +0530

    Palindromic add

--- src/Talentpace/PalindromicCheck.java
+++ src/Talentpace/PalindromicCheck.java
@@ -1,4 +1,36 @@
 package Talentpace;
 
 public class PalindromicCheck {
+
+    public static void main(String[] args) {
+
+        String str = "aba";
+
+        String res = palindrome(str);
+
+        if(str.equals(res)){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+
+    }
+
+    public static String palindrome(String str) {
+
+        String t = "";
+
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+
+            char ch = str.charAt(i);
+
+            t = t + ch;
+        }
+
+        return t;
+
+    }
+
+
 }
