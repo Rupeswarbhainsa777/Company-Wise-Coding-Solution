5a29e14452284dce5161ddd32c14e3c0ab54f092
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-08-26 23:17:06 +0530

    Largest element add

---
+++ src/StanverseTechnologies/LargestElement.java
@@ -0,0 +1,25 @@
+package StanverseTechnologies;
+
+public class LargestElement {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 9, 4, 5, 6};
+
+        largestNum(arr);
+
+    }
+
+    public static void largestNum(int arr[]) {
+
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+
+
+    }
+}
