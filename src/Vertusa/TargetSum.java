0abce5c4c672abd3ef2b771c27bdca91396d2919
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2026-09-03 21:03:50 +0530

    Target Sum completed

--- src/Vertusa/TargetSum.java
+++ src/Vertusa/TargetSum.java
@@ -3,9 +3,31 @@ package Vertusa;
 public class TargetSum {
     public static void main(String[] args) {
 
+        int arr[] = {1, 2, 3, 4};
+        targetSum(arr, 5);
     }
 
     public static void targetSum(int arr[], int target) {
+
+
+        int length = arr.length - 1;
+
+        for (int i = 1; i <= length; i++) {
+            for (int j = 0; j <= length - i; j++) {
+                int sum = 0;
+                for (int k = j; k < j + i; k++) {
+                    sum = sum + arr[k];
+                }
+                if (sum == target) {
+                    for (int k = j; k < j + i; k++) {
+                        System.out.print(arr[k] + " ");
+                    }
+                    System.out.println();
+
+                }
+            }
+        }
+
 
     }
 }
